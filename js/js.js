91b237a3a8163035c208c6c4e584e9fce9f4bc95
Ilias Karim <ilias.karim@gmail.com>
2012-03-11 18:30:00 -0500

    modified plot a little, added highest energy concerts

--- js/js.js
+++ js/js.js
@@ -1,39 +1,24 @@
 $(document).ready(function(){
-  var plot2 = $.jqplot ('chartdiv', [[3,7,9,1,5,3,8,2,5]], {
-      // Give the plot a title.
-      title: 'Plot With Options',
-      // You can specify options for all axes on the plot at once with
-      // the axesDefaults object.  Here, we're using a canvas renderer
-      // to draw the axis label which allows rotated text.
-      axesDefaults: {
-        labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-      },
-      // Likewise, seriesDefaults specifies default options for all
-      // series in a plot.  Options specified in seriesDefaults or
-      // axesDefaults can be overridden by individual series or
-      // axes options.
-      // Here we turn on smoothing for the line.
-      seriesDefaults: {
-          rendererOptions: {
-              smooth: true
-          }
-      },
-      // An axes object holds options for all axes.
-      // Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
-      // Up to 9 y axes are supported.
-      axes: {
-        // options for each axis are specified in seperate option objects.
-        xaxis: {
-          label: "X Axis",
-          // Turn off "padding".  This will allow data point to lie on the
-          // edges of the grid.  Default padding is 1.2 and will keep all
-          // points inside the bounds of the grid.
-          pad: 0
-        },
-        yaxis: {
-          label: "Y Axis"
-        }
-      }
-    });
+	var plot2 = $.jqplot ('chartdiv', 
+		[[3,7,9,1,5,3,8,2,5]], {
+		/*title: 'Energy Levels',*/
+		axesDefaults: {
+			labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+		},
+		seriesDefaults: {
+			rendererOptions: {
+				smooth: true
+			}
+		},
+		axes: {
+			xaxis: {
+				label: "Energy",
+				pad: 0
+			},
+			yaxis: {
+				label: "Time"
+			}
+		}
+	});
 });
 
